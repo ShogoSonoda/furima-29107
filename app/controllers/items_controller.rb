83e8963407eeae49136079f9dad544f7fc140417
sonoda <sonoponsyougo@gmail.com>
2020-10-28 20:40:45 +0900

    コントローラー・ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to 'root'
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_id, :shipment_source_id, :days_id, :price)
   end
 end
