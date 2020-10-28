9b13b41f39ddde9c22e74bb76d4d8a603d101be8
sonoda <sonoponsyougo@gmail.com>
2020-10-28 20:44:12 +0900

    itemsモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :day
+
+  validates :name, :description, :price, presence: true
+
+  validates :category_id, :status_id, :shipping_id, :shipment_source_id, :days_id, numericality: { other_than: 1 }
+
+  validates :price, numericality: { only_integer: true, greater_than: 300,less_than: 9999999 }
 end
