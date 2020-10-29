b15d17bcbd1c4360fcda5a44199d9c118e2a66f3
sonoda <sonoponsyougo@gmail.com>
2020-10-29 21:14:31 +0900

    itemのfactorybot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    image
+    name                 {"カメラ"}
+    description          {"綺麗な写真を撮れる"}
+    category_id          {6}
+    status_id            {2}
+    shipping_id          {2}
+    shipment_source_id   {2}
+    days_id              {2}
+    price                {5000}
+
+    association :user
   end
 end
