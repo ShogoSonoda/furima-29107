d588364be06770604618905269f703c82c9b4e9a
sonoda <sonoponsyougo@gmail.com>
2020-11-01 20:41:34 +0900

    ''の削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                 { 'カメラ' }
     description          { '写真が撮れる' }
-    category_id          { '6' }
-    status_id            { '2' }
-    shipping_id          { '2' }
-    shipment_source_id   { '2' }
-    days_id              { '2' }
-    price                { '5000' }
+    category_id          { 6 }
+    status_id            { 2 }
+    shipping_id          { 2 }
+    shipment_source_id   { 2 }
+    days_id              { 2 }
+    price                { 5000 }
     association :user
 
     after(:build) do |item|
