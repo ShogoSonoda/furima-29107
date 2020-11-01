7f10354d6a28a74e2e15a5b304df70a81ccb3b00
sonoda <sonoponsyougo@gmail.com>
2020-11-01 11:10:39 +0900

    販売手数料・販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const proFit = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTaxPrice.value = Math.floor(inputValue * 0.1)
+    const addTaxPriceValue = addTaxPrice.value
+    proFit.value = inputValue - addTaxPrice.value
+    const proFitValue = proFit.value
+    addTaxPrice.innerHTML = addTaxPriceValue
+    proFit.innerHTML = proFitValue
+  })
+});
